916f75c91813340e9985455c6b4dc483e225c106
mhmAdmin1122 <hasaanad12min@gmail.com>
2023-07-13 00:29:05 +0500

    Help page all View Set

--- pages/components/C/CallUs.tsx
+++ pages/components/C/CallUs.tsx
@@ -14,7 +14,7 @@ const CallUs = () => {
                 </div>
                 <div className="mapView">
                     <b>View Map Direction</b>
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d28957.57774046163!2d67.1055872!3d24.8741888!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2s!4v1689173017164!5m2!1sen!2s" width="600" height="450" loading="lazy" className='rounded-lg shadow-md shadow-gray-300 my-4'></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d28957.57774046163!2d67.1055872!3d24.8741888!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2s!4v1689173017164!5m2!1sen!2s" width="600" height="450" loading="lazy" className='rounded-lg shadow-md shadow-gray-300 my-4 map-view-frame'></iframe>
                 </div>
             </Link>
         </div>
