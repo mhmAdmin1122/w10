569085c7255484abfe5b8b694e00c1e6aaf1d5c3
mhmAdmin1122 <hasaanad12min@gmail.com>
2023-07-14 19:17:35 +0500

    tutorial and excercise page created

--- pages/components/T/Tutorial.tsx
+++ pages/components/T/Tutorial.tsx
@@ -1,8 +1,11 @@
 import Image from 'next/image'
 import React from 'react'
 import ProgrammingPic from '@/../public/img/programe.jpg'
-import dataentryPic from '../../../public/img/dataentry.jpg'
-import graphicPic from '../../../public/img/graphic.jpg'
+import dataentryPic from '@/../public/img/dataentry.jpg'
+import graphicPic from '@/../public/img/graphic.jpg'
+import databasePic from '@/../public/img/database.jpg'
+import HTL5PIC from '../../../public/img/html5.jpg'
+import nextpic from '../../../public/img/next.jpg'
 
 const Tutorial = () => {
   return (
@@ -59,6 +62,57 @@ const Tutorial = () => {
       <div className="more-btn w-full items-center flex justify-center">
         <button className='px-4 py-2 rounded-lg bg-gray-800 text-gray-50 font-medium flex items-center justify-center my-3'>For More Tutorials &rarr;</button>
       </div>
+
+      <div className="excercises-Bx px-12">
+
+        <b className='text-center w-full flex items-center justify-center my-4 text-2xl'>Excercises</b>
+
+        <div className="cards-box gap-4 my-3 flex items-start justify-center flex-wrap">
+
+          <div className="excercise-card w-80 rounded-lg shadow-lg shadow-gray-400 overflow-hidden">
+            <div className="cardPic-bx">
+              <Image src={databasePic} alt='card-pic' />
+            </div>
+            <div className="card-body px-2">
+              <b className='text-xl my-3 cursor-pointer'>Database Excercise</b>
+              <div className="more-btn w-full items-center flex justify-center">
+                <button className='px-4 py-2 rounded-lg bg-gray-800 text-gray-50 font-medium flex items-center justify-center my-3'>About More &rarr;</button>
+              </div>
+            </div>
+          </div>
+
+          <div className="excercise-card w-80 rounded-lg shadow-lg shadow-gray-400 overflow-hidden">
+            <div className="cardPic-bx">
+              <Image src={HTL5PIC} alt='card-pic' style={{height: 213.25}}/>
+            </div>
+            <div className="card-body px-2">
+              <b className='text-xl my-3 cursor-pointer'>HTML-5 Excercise</b>
+              <div className="more-btn w-full items-center flex justify-center">
+                <button className='px-4 py-2 rounded-lg bg-gray-800 text-gray-50 font-medium flex items-center justify-center my-3'>About More &rarr;</button>
+              </div>
+            </div>
+          </div>
+
+          <div className="excercise-card w-80 rounded-lg shadow-lg shadow-gray-400 overflow-hidden">
+            <div className="cardPic-bx">
+              <Image src={nextpic} alt='card-pic' />
+            </div>
+            <div className="card-body px-2">
+              <b className='text-xl my-3 cursor-pointer'>Next.js Excercise</b>
+              <div className="more-btn w-full items-center flex justify-center">
+                <button className='px-4 py-2 rounded-lg bg-gray-800 text-gray-50 font-medium flex items-center justify-center my-3'>About More &rarr;</button>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+      <div className="more-btn w-full items-center flex justify-center">
+        <button className='px-4 py-2 rounded-lg bg-gray-800 text-gray-50 font-medium flex items-center justify-center my-3'>For More Excercise &rarr;</button>
+      </div>
+
       <div className="space h-24"></div>
     </>
   )
