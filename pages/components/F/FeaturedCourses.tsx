7fe21e26c9c5eef33192b8121aec272256386b85
mhmAdmin1122 <hasaanad12min@gmail.com>
2023-08-07 23:17:52 +0500

    feature section updated successfully

--- pages/components/F/FeaturedCourses.tsx
+++ pages/components/F/FeaturedCourses.tsx
@@ -5,7 +5,7 @@ export default function FeaturedCourses() {
     return (
         <>
             <div className="teamCompo_Box grid justify-center justify-items-center bg-purple-400 px-16 py-14" >
-                <b className='font-bold text-2xl flex items-center gap-2 content-center'><MdOutlineFeaturedPlayList />Featured Courses</b>
+                <b className='font-bold text-2xl flex items-center gap-2 content-center text-gray-50'><MdOutlineFeaturedPlayList /><b>Featured Courses</b></b>
                 <div className="container relative z-0">
                     <Slide />
                 </div>
