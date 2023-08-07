6ee91c112a9617f4f0c394dddd68ff6108b3318f
mhmAdmin1122 <hasaanad12min@gmail.com>
2023-08-07 23:27:43 +0500

    About us page updated

--- pages/components/A/About.tsx
+++ pages/components/A/About.tsx
@@ -8,7 +8,7 @@ const About = () => {
   return (
     <section>
 
-      <div className="aboutus grid justify-center bg-blue-50 w-full px-10 py-12 h-auto">
+      <div className="aboutus grid justify-center bg-purple-200 w-full px-10 py-12 h-auto">
 
         <p className="text-orange-500 cursor-context-menu mt-2 text-4xl text-center font-bold">About Us</p>
 
@@ -21,17 +21,17 @@ const About = () => {
         </p>
 
         <div className="portions flex gap-8 mt-4 flex-wrap justify-center">
-          <div className="new-class h-auto w-80 rounded-3xl items-center px-5 pb-5 pt-12 shadow-lg shadow-gray-400 relative content-center bg-gray-200 grid justify-center justify-items-center">
+          <div className="new-class h-auto w-80 rounded-3xl items-center px-5 pb-5 pt-12 shadow-lg shadow-gray-400 relative content-center bg-purple-500 grid justify-center justify-items-center">
             <Image src={clasPic} alt="new-Class" className='w-20 h-20 border-2 border-gray-100 p-4 rounded-full' />
             <p className="text-center cursor-context-menu text-lg font-bold mt-3">NEW CLASSES</p>
             <p className="text-xl p-2 font-base text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, dolorem.</p>
           </div>
-          <div className="new-class h-auto w-80 rounded-3xl items-center px-5 pb-5 pt-12 shadow-lg shadow-gray-400 relative content-center bg-gray-200 grid justify-center justify-items-center">
+          <div className="new-class h-auto w-80 rounded-3xl items-center px-5 pb-5 pt-12 shadow-lg shadow-gray-400 relative content-center bg-purple-300 grid justify-center justify-items-center">
             <Image src={topCourses} alt="top-courses" className='w-20 h-20 border-2 border-gray-100 p-4 rounded-full' />
             <p className="text-center cursor-context-menu text-lg font-bold mt-3">TOP COURSES</p>
             <p className="text-xl p-2 font-base text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, dolorem.</p>
           </div>
-          <div className="new-class h-auto w-80 rounded-3xl items-center px-5 pb-5 pt-12 shadow-lg shadow-gray-400 relative content-center bg-gray-200 grid justify-center justify-items-center">
+          <div className="new-class h-auto w-80 rounded-3xl items-center px-5 pb-5 pt-12 shadow-lg shadow-gray-400 relative content-center bg-purple-500 grid justify-center justify-items-center">
             <Image src={ebooks} alt="E-BOOKS" className='w-20 h-20 border-2 border-gray-100 p-4 rounded-full' />
             <p className="text-center cursor-context-menu text-lg font-bold mt-3">FULL E-BOOKS</p>
             <p className="text-xl p-2 font-base text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, dolorem.</p>
