154bf2035311f4d8bba30fb29f156a40c2416fce
mhmAdmin1122 <hasaanad12min@gmail.com>
2023-08-08 00:46:49 +0500

    Footer Updated

--- pages/components/F/Footer.tsx
+++ pages/components/F/Footer.tsx
@@ -1,23 +1,29 @@
 import React from 'react'
 import Link from 'next/link'
-import { BiLogoFacebook, BiLogoInstagramAlt, BiLogoPinterest } from 'react-icons/bi'
-import { FaTiktok, FaYoutube } from 'react-icons/fa'
+import { BiLogoFacebook, BiLogoInstagramAlt, BiLogoPinterest, BiSolidEnvelope } from 'react-icons/bi'
+import { FaTiktok, FaYoutube, FaBlenderPhone } from 'react-icons/fa'
+import { MdEscalatorWarning, MdFollowTheSigns, MdContacts } from 'react-icons/md';
+import { GiUfo, GiDirectionSign } from 'react-icons/gi';
 
 const Footer = () => {
     return (
-        <footer className="w-full h-auto bg-purple-900 flex justify-around py-6">
+        <footer className="w-full h-auto bg-purple-200 flex justify-around py-6">
 
             <div className="grid justify-center items-center">
 
-                <p className="font-extrabold text-2xl cursor-context-menu text-red-600">
-                    ABOUT US
+                <p className="font-extrabold text-2xl cursor-context-menu text-orange-500 flex items-center gap-3">
+                    <b><MdEscalatorWarning /></b>
+                    <b>ABOUT US</b>
                 </p>
 
-                <p className='text-gray-50 pt-3 w-60 text-justify'>
+                <p className='text-black pt-3 w-60 text-justify'>
                     Welcome to our education site! At world10, we believe in empowering minds and shaping futures.
                 </p>
 
-                <b className="font-extrabold text-2xl cursor-context-menu text-red-600 mt-4">Follow Us Now!</b>
+                <b className="font-extrabold text-2xl cursor-context-menu text-orange-500 mt-4 flex items-center gap-3">
+                    <b><MdFollowTheSigns /></b>
+                    <b>Follow Us Now!</b>
+                </b>
 
                 <div className="social-icons flex gap-2 mt-4">
                     <Link href={"/"} className='bg-purple-600 text-white p-2 rounded-full text-xl border-2 border-purple-600 hover:border-2 hover:border-purple-600 hover:bg-purple-300 hover:text-purple-800' title=''><FaTiktok /></Link>
@@ -30,28 +36,41 @@ const Footer = () => {
 
             <div className="recent">
 
-                <p className='font-extrabold text-2xl cursor-context-menu text-red-600'>RECENT POSTS</p>
+                <p className='font-extrabold text-2xl cursor-context-menu text-orange-500 flex items-center gap-3'>
+                    <b><GiUfo /></b>
+                    <b>Famous Courses</b>
+                </p>
 
                 <div className="pics pt-3">
 
-                    <p className='flex gap-2 text-gray-50'>
+                    <p className='flex gap-2 text-black cursor-pointer items-center'>
                         <b className='font-bold text-xl text-red-600'>&rarr;</b>
-                        <b> Access to 4,000+ of our top courses</b>
+                        <p>Angular Course With Certificate</p>
                     </p>
 
-                    <p className='flex gap-2 text-gray-50'>
+                    <p className='flex gap-2 text-black cursor-pointer items-center'>
                         <b className='font-bold text-xl text-red-600'>&rarr;</b>
-                        <b> Explore a variety of fresh topics</b>
+                        <b>React Course With Certificate</b>
                     </p>
 
-                    <p className='flex gap-2 text-gray-50'>
+                    <p className='flex gap-2 text-black cursor-pointer items-center'>
                         <b className='font-bold text-xl text-red-600'>&rarr;</b>
-                        <b> Find the right instructor for you</b>
+                        <p>Python Course With Certificate</p>
                     </p>
 
-                    <p className='flex gap-2 text-gray-50'>
+                    <p className='flex gap-2 text-black cursor-pointer items-center'>
                         <b className='font-bold text-xl text-red-600'>&rarr;</b>
-                        <b> Find the right instructor for you</b>
+                        <b>MongoDB Course With Certificate</b>
+                    </p>
+
+                    <p className='flex gap-2 text-black cursor-pointer items-center'>
+                        <b className='font-bold text-xl text-red-600'>&rarr;</b>
+                        <p>Node.Js Course With Certificate</p>
+                    </p>
+
+                    <p className='flex gap-2 text-black cursor-pointer items-center'>
+                        <b className='font-bold text-xl text-red-600'>&rarr;</b>
+                        <b>Express.Js Course With Certificate</b>
                     </p>
 
                 </div>
@@ -60,20 +79,32 @@ const Footer = () => {
 
             <div className="address grid">
 
-                <p className='font-extrabold text-2xl cursor-context-menu text-red-600'>CONTACT US</p>
+                <p className='font-extrabold text-2xl cursor-context-menu text-orange-500 flex items-center gap-3'>
+                    <b><MdContacts /></b>
+                    <b>CONTACT US</b>
+                </p>
 
-                <b className="text-gray-50 pt-3">
-                    <b>Address:</b>
+                <b className="text-black pt-3">
+                    <p className='flex items-center gap-2'>
+                        <b><GiDirectionSign /></b>
+                        <b>Address:</b>
+                    </p>
                     <address>Bahawalpur, PB, Pakistan, 63100</address>
                 </b>
 
-                <b className="text-gray-50 pt-3 grid">
-                    <b>Phone:</b> 
-                    <code>+9203257390045</code>
+                <b className="text-black pt-3 grid">
+                    <p className='flex items-center gap-2'>
+                        <b><FaBlenderPhone /></b>
+                        <b>Phone:</b>
+                    </p>
+                    <code>+923257390045</code>
                 </b>
 
-                <b className="text-gray-50 pt-3 grid">
-                    <b>Email:</b> 
+                <b className="text-black pt-3 grid">
+                    <p className='flex items-center gap-2'>
+                        <b><BiSolidEnvelope /></b>
+                        <b>Email:</b>
+                    </p>
                     <code>w10closetoworld@gmail.com</code>
                 </b>
 
